eb1507f8c97e4d0de187ef6141ff0c9170b91675
beluga <xiandi_shan@outlook.jp>
2023-05-08 05:43:14 +0900

    update

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import ImageMessage, ImageSendMessage
+from reservation import Reservation
+
+TOKEN = "TZvX2e2N777ZZWUbxIZ0N5/4sVkGKVMyVzJ0hBuPUN3VXWu750bWrwhbQzoRy4nx24H1Sa38knAssjl/lYJVUbseTor9jK1UnQOAEYOFAW1j+bq2D1ae213zuMeOWc5YfPz3OSK1CNOhNspkxeuQGAdB04t89/1O/w1cDnyilFU="
+SECRET = "d6d87ccdad6f7b412cc6ce0bccd92186"
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(TOKEN)
+handler = WebhookHandler(SECRET)
+reservation = Reservation()
+
+# endpoint
+@app.route("/")
+def test():
+    return "<h1>It Works!</h1>"
+
+# endpoint from linebot
+@app.route("/callback", methods=['GET', 'POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    input = event.message.text
+    if not reservation.check(input[4:6], input[6::]):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="空きがないよー")
+        )
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="空いているよー")
+        )
+
+if __name__ == "__main__":
+    # app.run(port=int(os.getenv('PORT', 5002)))
+    app.run()
---
+++ reservation.py
@@ -0,0 +1,57 @@
+import time
+import chromedriver_binary
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+class Reservation:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        self.driver.get('http://www.net.city.nagoya.jp/sporec/index.html')
+
+
+    def check(self, input_month, input_day):
+        place_search = self.driver.find_element(By.XPATH, '//a[@href="https://www.net.city.nagoya.jp/cgi-bin/sp04001"]')
+        place_search.click()
+
+        # 種目の選択
+        event = self.driver.find_element(By.NAME, 'syumoku')
+        event_select = Select(event)
+        event_select.select_by_value('024')
+
+        # 利用日の選択
+        month = self.driver.find_element(By.NAME, 'month')
+        month_select = Select(month)
+        month_select.select_by_value(input_month)
+
+        day = self.driver.find_element(By.NAME, 'day')
+        day_select = Select(day)
+        day_select.select_by_value(input_day)
+
+        # 希望条件の選択
+        # radio_button = driver.find_element(By.XPATH, '//*[@id="main"]/table[2]/tbody/tr[2]/td[4]/input[2]')
+        # radio_button.click()
+
+        # 供用区分の選択
+        usage = self.driver.find_element(By.NAME, 'kyoyo1')
+        usage_select = Select(usage)
+        usage_select.select_by_value('07')
+
+        # 照会
+        submit = self.driver.find_element(By.XPATH, '//*[@id="main"]/table[2]/tbody/tr[4]/td[3]/input[15]')
+        submit.click()
+
+        # 空きがあるかどうかをチェックする
+        try:
+            self.driver.find_element(By.CLASS_NAME, 'ERRLABEL1')
+            text = "空きがありません"
+            return False
+        except:
+            # スクショを撮る
+            self.driver.save_screenshot("screenshot.png")
+            self.driver.quit()
+            return True
