371ab85b4c79d8dc3ef36336ef28b54f0e38ac36
beluga <xiandi_shan@outlook.jp>
2023-07-01 16:07:30 +0900

    update

--- reservation.py
+++ reservation.py
@@ -1,5 +1,4 @@
 import os
-import chromedriver_binary
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
